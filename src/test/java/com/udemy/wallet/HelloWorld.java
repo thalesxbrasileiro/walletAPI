c6ec0885c46da3308fc430bc004db5ab932f9d63
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2025-09-22 23:15:18 -0300

    Adicionada as configurações iniciais para os testes

---
+++ src/test/java/com/udemy/wallet/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.udemy.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class HelloWorld {
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals(1, 1);
+    }
+
+}
